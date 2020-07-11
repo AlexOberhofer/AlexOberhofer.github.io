34a403e89d96c0701f7eb0d48256ed6aa48a91e5
Alex Oberhofer <alexmoberhofer@gmail.com>
2020-07-11 11:31:22 -0400

    Menu Responsiveness

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -8,6 +8,14 @@ function random(min, max)
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+/* Do the Menu Transformation for mobile */
+function doMenuMagic(window, document) {
+    document.getElementById('toggle').addEventListener('click', function (e) {
+        document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+        document.getElementById('toggle').classList.toggle('x');
+    });
+}
+
 function decideImage() 
 {
     var img = 0;
