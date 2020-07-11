7c346d38d7754929a66f2177f0d543a31486ef1c
Alex Oberhofer <alexmoberhofer@gmail.com>
2020-07-11 17:00:31 -0400

    add new warning bar and start using icons

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -15,27 +15,3 @@ function doMenuMagic(window, document) {
         document.getElementById('toggle').classList.toggle('x');
     });
 }
-
-function decideImage() 
-{
-    var img = 0;
-    var src = "";
-
-    if(img == 0) {
-        src =  "transparent url('https://raw.githubusercontent.com/AlexOberhofer/web-images/master/banner.jpg') 0 0 no-repeat fixed"
-    } 
-
-    if(img == 1) {
-        src =  "transparent url('https://raw.githubusercontent.com/AlexOberhofer/web-images/master/banner-2.jpg') 0 0 no-repeat fixed"
-    }
-
-    if (img == 3) {
-        src =  "transparent url('https://raw.githubusercontent.com/AlexOberhofer/web-images/master/banner-3.jpg') 0 0 no-repeat fixed"
-    }
-
-    
-1
-
-$( "#banner-img" ).css('background', src);
-
-}
